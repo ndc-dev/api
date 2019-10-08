eb650b0cdd22980858daa777aef99e125f264669
Satoshi Deguchi <deguchik@gmail.com>
2019-10-08 14:04:11 +0900

    update

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-SET GOOGLE_APPLICATION_CREDENTIALS=X:\ndc-dev-api.json.json & gcloud config set project ndc-dev-255301 & gcloud builds submit --tag gcr.io/ndc-dev-255301/api & gcloud beta run deploy --image gcr.io/ndc-dev-255301/api:latest --platform managed
+SET GOOGLE_APPLICATION_CREDENTIALS=X:\ndc-dev-api.json.json & gcloud config set project ndc-dev-255301 & gcloud builds submit --tag gcr.io/ndc-dev-255301/api & gcloud beta run deploy --image gcr.io/ndc-dev-255301/api:latest --platform managed --memory 512
